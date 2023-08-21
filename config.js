7cecac53478f0b9d45e23605ebd4226f1c98d6f9
Nicholas Wong <lion9200.world@gmail.com>
2023-08-21 14:41:16 -0700

    added auth

---
+++ config.js
@@ -0,0 +1,4 @@
+const accessKeyId = "AKIAWPL2H3XTKGXLIPF6";
+const secretAccessKey = "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";
+const email = "shubhamdwivediunique@gmail.com";
+const password = "@Qwerty1998";
--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
+import "@aws-amplify/ui-react/styles.css";
 import './App.css';
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card
+} from "@aws-amplify/ui-react";
 
-function App() {
+const App = ({ signOut }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello Amazon Web Service from Admiral9200</h1>
-      </header>
-    </div>
+    <View className='App'>
+      <Card>
+        <Image src={ logo } className='App-logo' alt='logo' />
+        <Heading level={1}>We now have Auth!</Heading>
+      </Card>
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
